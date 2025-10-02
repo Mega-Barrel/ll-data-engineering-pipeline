8c76f1cf05cfb6bf84fbfded80ad08579023abc9
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2025-10-02 22:16:59 +0530

    removed unnecessary imports

--- setup.py
+++ setup.py
@@ -5,16 +5,7 @@ setup(
     name = "modern-data-stack-project",
     packages = find_packages(),
     install_requires = [
-        "dbt-bigquery",
-        "dagster",
-        "dagster-cloud",
-        "dagster-dbt",
-        "dagster-airbyte",
+        "dbt-postgres",
+        "dagster-dbt"
     ],
-    extras_require = {
-        "dev": [
-            "dagit",
-            "pytest"
-        ]
-    },
 )
