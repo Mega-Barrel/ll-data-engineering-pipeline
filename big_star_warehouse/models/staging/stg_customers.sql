48abcd921ff6bc002cc07fa57dcf5896103daeb5
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2025-10-04 22:25:26 +0530

    Created staging customers model

--- big_star_warehouse/models/staging/stg_customers.sql
+++ big_star_warehouse/models/staging/stg_customers.sql
@@ -1,11 +1,26 @@
 
+WITH stg_customers AS (
+    SELECT
+        customer_id,
+        first_name,
+        last_name,
+        CONCAT(first_name, ' ', last_name) AS full_name,
+        email,
+        gender,
+        city,
+        country,
+        ip_address
+    FROM
+        {{ source('raw_data', 'customers') }}
+)
 
 SELECT
     customer_id,
+    full_name,
     email,
     gender,
     city,
     country,
     ip_address
 FROM
-    {{ source('raw_data', 'customers') }}
+    stg_customers
